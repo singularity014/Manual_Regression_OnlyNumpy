b57172cd30fd9ba728e24f1b36c4a615d70f0380
singularity014 <49594316+singularity014@users.noreply.github.com>
2019-04-14 11:25:20 +0200

    Add files via upload

---
+++ manual_regression.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Author - Praful Sharma
+
+# # Simple Linear Regression. Only using 
+
+import numpy as np
+
+# matplotlib and mpl_toolkits are not necessary. We employ them for the sole purpose of visualizing the results.  
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+# GENERATE INPUT OBSERVATIONS (two variable x and z)
+
+# First, we should declare a variable containing the size of the training set we want to generate.
+observations = 1000
+
+# We will work with two variables as inputs.
+# We generate them randomly, drawing from an uniform distribution
+# The size of xs and zs is observations by 1. In this case: 1000 x 1.
+xs = np.random.uniform(low=-10, high=10, size=(observations,1))
+zs = np.random.uniform(-10, 10, (observations,1))
+
+# Combine the two dimensions of the input into one input matrix. 
+# This is the X matrix from the linear model y = x*w + b.
+# column_stack is a Numpy method, which combines two vectors into a matrix. Alternatives are stack, dstack, hstack, etc.
+inputs = np.column_stack((xs,zs))
+
+# Check if the dimensions of the inputs are the same as the ones we defined in the linear model lectures. 
+# They should be n x k, where n is the number of observations, and k is the number of variables, so 1000 x 2.
+print (inputs.shape)
+
+
+# GENERATE TARGETS --
+
+# We want to "make up" a function, use the ML methodology, and see if the algorithm has learned it.
+# We add a small random noise to the function i.e. f(x,z) = 2x - 3z + 5 + <small noise>
+noise = np.random.uniform(-1, 1, (observations,1))
+
+# Produce the targets according to the f(x,z) = 2x - 3z + 5 + noise definition.
+# In this way, we are basically saying: the weights should be 2 and -3, while the bias is 5.
+targets = 2*xs - 3*zs + 5 + noise
+
+# Check the shape of the targets just in case. It should be n x m, where m is the number of output variables, so 1000 x 1.
+print (targets.shape)
+
+
+# DATA VISUALIZATION
+# The point is to see that there is a strong trend that our model should learn to reproduce.
+# In order to use the 3D plot, the objects should have a certain shape, so we reshape the targets.
+# The proper method to use is reshape and takes as arguments the dimensions in which we want to fit the object.
+targets = targets.reshape(observations,)
+
+# Plotting according to the conventional matplotlib.pyplot syntax
+
+# Declare the figure
+fig = plt.figure()
+
+# A method allowing us to create the 3D plot
+ax = fig.add_subplot(111, projection='3d')
+
+# Choose the axes.
+ax.plot(xs, zs, targets)
+
+# Set labels
+ax.set_xlabel('xs')
+ax.set_ylabel('zs')
+ax.set_zlabel('Targets')
+
+# You can fiddle with the azim parameter to plot the data from different angles. Just change the value of azim=100
+# to azim = 0 ; azim = 200, or whatever. Check and see what happens.
+ax.view_init(azim=100)
+
+# So far we were just describing the plot. This method actually shows the plot. 
+plt.show()
+
+# We reshape the targets back to the shape that they were in before plotting.
+# This reshaping is a side-effect of the 3D plot. Sorry for that.
+targets = targets.reshape(observations,1)
+
+
+# INTIALIZE WEIGHTS AND BIASES
+
+# We will initialize the weights and biases randomly in some small initial range.
+# Note that High initial ranges may prevent the machine learning algorithm from learning.
+init_range = 0.1
+
+# Weights are of size k x m, where k is the number of input variables and m is the number of output variables
+# In our case, the weights matrix is 2x1 since there are 2 inputs (x and z) and one output (y)
+weights = np.random.uniform(low=-init_range, high=init_range, size=(2, 1))
+
+# Biases are of size 1 since there is only 1 output. The bias is a scalar.
+biases = np.random.uniform(low=-init_range, high=init_range, size=1)
+
+#Print the weights to get a sense of how they were initialized.
+print (weights)
+print (biases)
+
+
+# LEARNING RATE INITIALIZATION
+
+# Set some small learning rate (denoted eta in the lecture). 
+# 0.02 is going to work quite well for our example. Once again, you can play around with it.
+# It is HIGHLY recommended that you play around with it.
+learning_rate = 0.02
+
+
+# TRAIN THE MODEL (OPTIMIZATION OF LOSS FUNCTION WITH UPDATE RULE )
+
+# We iterate over our training dataset 100 times. That works well with a learning rate of 0.02.
+# The proper number of iterations is something we will talk about later on, but generally
+# a lower learning rate would need more iterations, while a higher learning rate would need less iterations
+# keep in mind that a high learning rate may cause the loss to diverge to infinity, instead of converge to 0.
+for i in range (100):
+    
+    # This is the linear model: y = xw + b equation
+    outputs = np.dot(inputs,weights) + biases
+    # The deltas are the differences between the outputs and the targets
+    # Note that deltas here is a vector 1000 x 1
+    deltas = outputs - targets
+        
+    # We are considering the L2-norm loss, but divided by 2, so it is consistent with the lectures.
+    # Moreover, we further divide it by the number of observations.
+    # This is simple rescaling by a constant. We explained that this doesn't change the optimization logic,
+    # as any function holding the basic property of being lower for better results, and higher for worse results
+    # can be a loss function.
+    loss = np.sum(deltas ** 2) / 2 / observations
+    
+    # We print the loss function value at each step so we can observe whether it is decreasing as desired.
+    print (loss)
+    
+    # Another small trick is to scale the deltas the same way as the loss function
+    # In this way our learning rate is independent of the number of samples (observations).
+    # Again, this doesn't change anything in principle, it simply makes it easier to pick a single learning rate
+    # that can remain the same if we change the number of training samples (observations).
+    # You can try solving the problem without rescaling to see how that works for you.
+    deltas_scaled = deltas / observations
+    
+    # Finally, we must apply the gradient descent update rules from the relevant lecture.
+    # The weights are 2x1, learning rate is 1x1 (scalar), inputs are 1000x2, and deltas_scaled are 1000x1
+    # We must transpose the inputs so that we get an allowed operation.
+    weights = weights - learning_rate * np.dot(inputs.T,deltas_scaled)
+    biases = biases - learning_rate * np.sum(deltas_scaled)
+    
+    # The weights are updated in a linear algebraic way (a matrix minus another matrix)
+    # The biases, however, are just a single number here, so we must transform the deltas into a scalar.
+    # The two lines are both consistent with the gradient descent methodology. 
+
+
+# CHECK RESULTS :) 
+
+# We print the weights and the biases, so we can see if they have converged to what we wanted.
+# When declared the targets, following the f(x,z), we knew the weights should be 2 and -3, while the bias: 5.
+print (weights, biases)
+
+# Note that they may be convergING. So more iterations are needed.
+
+
+# Plot last outputs vs targets
+# Since they are the last ones at the end of the training, they represent the final model accuracy. <br/>
+# The closer this plot is to a 45 degree line, the closer target and output values are.
+
+
+# We print the outputs and the targets in order to see if they have a linear relationship.
+# Again, that's not needed. Moreover, in later lectures, that would not even be possible.
+plt.plot(outputs,targets)
+plt.xlabel('outputs')
+plt.ylabel('targets')
+plt.show()
+
+
